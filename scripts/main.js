f9a2b9a00894482d7c4202cde951ca94e3c4eca9
Nessad Choudhury <nessadchoudhury@gmail.com>
2021-03-17 14:40:06 -0400

    adding change text color feature

--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,11 @@ myImage.onclick = function() {
     }
 }
 
-let myButton = document.querySelector('button');
+let myButton = document.querySelector('button[name=username]');
+let textColorButton = document.querySelector('button[name=text_color]');
 let myHeading = document.querySelector('h1');
+let myTexts = document.querySelectorAll('p');
+let myLists = document.querySelectorAll('li')
 
 function setUserName() {
     let myName = prompt('Please enter your name.');
@@ -20,7 +23,24 @@ function setUserName() {
         localStorage.setItem('name', myName);
         myHeading.innerHTML = 'Hello, ' + myName;
     }
-  }
+}
+
+function changeTextColor() {
+    myTexts.forEach(function(text) {
+        if(text.style.color != 'blue') {
+            text.style.color = "blue";
+        } else {
+            text.style.color = '#F6F740'
+        }
+    });
+    myLists.forEach(function(list) {
+        if(list.style.color != 'blue') {
+            list.style.color = "blue";
+        } else {
+            list.style.color = '#F6F740'
+        }
+    });
+}
 
 if(!localStorage.getItem('name')) {
     setUserName();
@@ -31,4 +51,8 @@ if(!localStorage.getItem('name')) {
 
 myButton.onclick = function() {
     setUserName();
+}
+
+textColorButton.onclick = function() {
+    changeTextColor();
 }
