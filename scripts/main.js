1adbbd7e70e92d1e59852109fc01a26e5206d632
Nessad Choudhury <nessadchoudhury@gmail.com>
2021-03-17 13:23:31 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/audi.jpg') {
+        myImage.setAttribute('src', 'images/nissan.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/audi.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Hello, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello, ' + myName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
